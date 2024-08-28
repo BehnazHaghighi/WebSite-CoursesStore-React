b1847a4a51de799a369da495f1592138a5623990
Behnaz Haghighi <behnaz18093@gmail.com>
2024-08-28 13:51:07 +0330

    create ReadMore product in home page

---
+++ src/components/ReadMore/ReadMore.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getProdacts } from "../../servises/todoService";
+import ProductCard from "../ProductCard/ProductCard";
+
+const ReadMore = () => {
+  const [ProdactsList, setProdactsList] = useState([]);
+  
+  // get data
+  const getProdactsHandler = async () => {
+    try {
+      const response = await getProdacts();
+      setProdactsList(response.products);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    getProdactsHandler();
+    return () => controller.abort();
+  }, []);
+
+  // only 6 product
+  const firstSixProdacts = ProdactsList.slice(0, 6);
+
+  return (
+    <div className="container mx-auto p-4">
+      {/*show 6 product*/}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {firstSixProdacts.map((item) => (
+          <ProductCard
+            key={item.id}
+            image={item?.thumbnail}
+            altname={item?.title}
+            productname={item?.title}
+            price={item?.price}
+            linkproduct={`/ProdactDetail/${item.id}`}
+          />
+        ))}
+      </div>
+
+      {/* دکمه Read More */}
+      <div className="text-center mt-8">
+        <Link
+          to="/Prodacts"
+          className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
+        >
+          مشاهده ی همه ی دوره های آموزشی
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ReadMore;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,97 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import {
+  FaPencilAlt,
+  FaGraduationCap,
+  FaVideo,
+  FaFolderOpen,
+  FaLightbulb,
+  FaListAlt,
+  FaClock,
+  FaDollarSign,
+} from "react-icons/fa";
+
+import pic from "../../assets/image/masood.jpg";
+import { Link } from "react-router-dom";
+import ReadMore from "../../components/ReadMore/ReadMore";
 
 const Home = () => {
   return (
     <>
       <Layout>
-        <h2>Welcome to the Home Page!</h2>
+        {/* section1 information site */}
+        <div className="container mx-auto p-4 md:p-8 rtl">
+          <div className="flex flex-col justify-between items-center md:flex-row bg-white shadow-lg rounded-lg overflow-hidden">
+            {/* Section for home Info */}
+            <div className="w-full md:w-1/2 p-4 md:p-8">
+              <h6 className="text-2xl font-bold text-gray-900 mb-4">
+                برنامه نویسی به زبان ساده.
+              </h6>
+              <p>
+                در فرانت کست می‌توانید مهارت‌های برنامه نویسی خود را تقویت کنید
+                و به یک توسعه‌دهنده حرفه‌ای تبدیل شوید.
+              </p>
+
+              <Link to={"/Prodacts"}>
+                <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">
+                  شروع یادگیری
+                </button>
+              </Link>
+
+              <div className="flex flex-col space-y-4 text-right mt-5">
+                {/* icon section */}
+                <div className="flex flex-col space-y-4">
+                  <div className="flex flex-row">
+                    <div className="flex items-center space-x-2">
+                      <FaPencilAlt size={14} className="text-blue-500" />
+                      <span className="text-xs">یادگیری با انجام تمرین</span>
+                    </div>
+
+                    <div className="flex items-center space-x-2">
+                      <FaClock size={14} className="text-blue-500" />
+                      <span className="text-xs">ویدیوهای کوتاه و با کیفیت</span>
+                    </div>
+
+                    <div className="flex items-center space-x-2">
+                      <FaFolderOpen size={14} className="text-blue-500" />
+                      <span className="text-xs">ضمانت بازگشت وجه</span>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-row">
+                    <div className="flex items-center space-x-2">
+                      <FaVideo size={14} className="text-blue-500" />
+                      <span className="text-xs">به روز رسانی رایگان </span>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <FaListAlt size={24} className="text-blue-500" />
+                      <span className="text-xs">سرفصل‌های جامع و دقیق</span>
+                    </div>
+
+                    <div className="flex items-center space-x-2">
+                      <FaLightbulb size={24} className="text-blue-500" />
+                      <span className="text-xs">پشتیبانی دوره‌ها </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Section for Image */}
+            <div>
+              <img src={pic} alt="pic" className="w-full h-64 object-contain" />
+            </div>
+          </div>
+        </div>
+        {/* end section1 information site */}
+
+        {/* section 2 readmore product */}
+        <div>
+          <h1 className="text-2xl font-bold mb-6">دوره‌های آموزشی</h1>
+          <ReadMore />
+        </div>
+
+        {/* end section 2 readmore product */}
       </Layout>
     </>
   );
